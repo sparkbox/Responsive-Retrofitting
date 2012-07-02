297ca6eb928b8797a64ddad2a16efdd23320dec4
Ben Callahan <ben@heysparkbox.com>
2012-07-02 15:20:22 -0400

    making directories for each domain
    this will allow users to have their own images and easier organization
    for pre compilers…

--- responsive-retrofit.js
+++ responsive-retrofit.js
@@ -1,5 +1,5 @@
 var cssLink = document.createElement('link');
 cssLink.setAttribute('rel', 'stylesheet');
 cssLink.setAttribute('type', 'text/stylesheet');
-cssLink.setAttribute('href', "http://sparkbox.github.com/Responsive-Retrofitting/" + document.location.hostname + ".css");
+cssLink.setAttribute('href', "http://sparkbox.github.com/Responsive-Retrofitting/" + document.location.hostname + "/retrofit.css");
 document.head.appendChild(cssLink);
