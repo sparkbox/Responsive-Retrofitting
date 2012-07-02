ffb6fdd7bbdfe4f77d094fcd0d8e32fdc9f5af44
Ben Callahan <ben@heysparkbox.com>
2012-07-02 14:27:23 -0400

    writing link into head with url-based css file

--- responsive-retrofit.js
+++ responsive-retrofit.js
@@ -1,2 +1,4 @@
-alert(document.location.hostname);
-
+var cssLink = document.createElement('link');
+cssLink.setAttribute('rel', 'stylesheet');
+cssLink.setAttribute('href', "https://raw.github.com/sparkbox/Responsive-Retrofitting/master/" + document.location.hostname + ".css");
+document.head.appendChild(cssLink);
