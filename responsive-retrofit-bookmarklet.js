ca9f5d2059877f7c908d4e96ccc8b7d76a960927
Ben Callahan <ben@heysparkbox.com>
2012-07-02 14:42:19 -0400

    removing prototcol

--- responsive-retrofit-bookmarklet.js
+++ responsive-retrofit-bookmarklet.js
@@ -1,6 +1,6 @@
 javascript: (function () {  
   var jsCode = document.createElement('script');
 
-  jsCode.setAttribute('src', 'http://sparkbox.github.com/Responsive-Retrofitting/responsive-retrofit.js');
+  jsCode.setAttribute('src', '//sparkbox.github.com/Responsive-Retrofitting/responsive-retrofit.js');
   document.body.appendChild(jsCode);  
 }());
--- responsive-retrofit.js
+++ responsive-retrofit.js
@@ -1,5 +1,5 @@
 var cssLink = document.createElement('link');
 cssLink.setAttribute('rel', 'stylesheet');
 cssLink.setAttribute('type', 'text/stylesheet');
-cssLink.setAttribute('href', "http://sparkbox.github.com/Responsive-Retrofitting/" + document.location.hostname + ".css");
+cssLink.setAttribute('href', "//sparkbox.github.com/Responsive-Retrofitting/" + document.location.hostname + ".css");
 document.head.appendChild(cssLink);
