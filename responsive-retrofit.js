7d26e288cd1cc3a81d00d6b9e51dcd75efd9919d
Ben Callahan <ben@heysparkbox.com>
2012-07-02 14:29:46 -0400

    add type attribute to link

--- responsive-retrofit.js
+++ responsive-retrofit.js
@@ -1,4 +1,5 @@
 var cssLink = document.createElement('link');
 cssLink.setAttribute('rel', 'stylesheet');
+cssLink.setAttribute('type', 'text/stylesheet');
 cssLink.setAttribute('href', "https://raw.github.com/sparkbox/Responsive-Retrofitting/master/" + document.location.hostname + ".css");
 document.head.appendChild(cssLink);
