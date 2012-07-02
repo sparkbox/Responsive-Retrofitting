5885aa36c691da2eab2c3451b36696e6fbe99be0
Ben Callahan <ben@heysparkbox.com>
2012-07-02 14:36:56 -0400

    adjusting URL refs gh-pages

--- responsive-retrofit.js
+++ responsive-retrofit.js
@@ -1,5 +1,5 @@
 var cssLink = document.createElement('link');
 cssLink.setAttribute('rel', 'stylesheet');
 cssLink.setAttribute('type', 'text/stylesheet');
-cssLink.setAttribute('href', "https://raw.github.com/sparkbox/Responsive-Retrofitting/master/" + document.location.hostname + ".css");
+cssLink.setAttribute('href', "https://sparkbox.github.com/Responsive-Retrofitting/" + document.location.hostname + ".css");
 document.head.appendChild(cssLink);
