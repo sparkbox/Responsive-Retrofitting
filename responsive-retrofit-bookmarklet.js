bfb123bf12ebb5c2b56f5c6125f79f1c9a9b5eb7
Ben Callahan <ben@heysparkbox.com>
2012-07-02 14:39:53 -0400

    pointing urls to gh-pages

--- responsive-retrofit-bookmarklet.js
+++ responsive-retrofit-bookmarklet.js
@@ -1,6 +1,6 @@
 javascript: (function () {  
   var jsCode = document.createElement('script');
 
-  jsCode.setAttribute('src', 'https://raw.github.com/sparkbox/responsive-retrofitting/master/responsive-retrofit.js');
+  jsCode.setAttribute('src', 'http://sparkbox.github.com/Responsive-Retrofitting/responsive-retrofit.js');
   document.body.appendChild(jsCode);  
 }());
