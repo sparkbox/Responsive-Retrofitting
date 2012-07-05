925a81f012a81e5dfedc8050d74811b740005903
Andy Rossi <aroccorossi@gmail.com>
2012-07-05 13:21:29 -0400

    Ok, for realsies this time.

--- responsive-retrofit-bookmarklet.js
+++ responsive-retrofit-bookmarklet.js
@@ -1,12 +1,5 @@
 javascript: (function () {  
   var jsCode = document.createElement('script');
   jsCode.setAttribute('src', 'http://sparkbox.github.com/Responsive-Retrofitting/responsive-retrofit.js');
-  document.body.appendChild(jsCode);
-  
-  // Add meta tag, too!
-  var metaCode = document.createElement('meta');
-  metaCode.setAttribute('name', 'viewport');
-  metaCode.setAttribute('content', 'width=device-width,initial-scale=1.0');
-  document.head.appendChild(metaCode);
-  
+  document.body.appendChild(jsCode);  
 }());
--- responsive-retrofit.js
+++ responsive-retrofit.js
@@ -3,3 +3,10 @@ cssLink.setAttribute('rel', 'stylesheet');
 cssLink.setAttribute('type', 'text/stylesheet');
 cssLink.setAttribute('href', "http://sparkbox.github.com/Responsive-Retrofitting/" + document.location.hostname + "/css/retrofit.css");
 document.head.appendChild(cssLink);
+
+
+// Add meta tag, too!
+var metaCode = document.createElement('meta');
+metaCode.setAttribute('name', 'viewport');
+metaCode.setAttribute('content', 'width=device-width,initial-scale=1.0');
+document.head.appendChild(metaCode);
