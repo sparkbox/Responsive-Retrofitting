db43f511fc8098d1112220992ccbb57a226dbb65
Philip <philip@phanza.com>
2012-07-06 23:14:04 -0400

    Corrected javascript link

--- responsive-retrofit-bookmarklet.js
+++ responsive-retrofit-bookmarklet.js
@@ -1,5 +1,5 @@
 javascript: (function () {  
   var jsCode = document.createElement('script');
-  jsCode.setAttribute('src', 'https://raw.github.com/phanza/Responsive-Retrofitting/gh-pages/responsive-retrofit-bookmarklet.js');
+  jsCode.setAttribute('src', 'https://raw.github.com/phanza/Responsive-Retrofitting/gh-pages/responsive-retrofit.js');
   document.body.appendChild(jsCode);  
 }());
