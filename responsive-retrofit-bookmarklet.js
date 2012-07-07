376640d2426402b2b4f7395e71254a549794e0c0
Philip <philip@phanza.com>
2012-07-06 23:44:23 -0400

    Updated links to branch. Hopefully.
    May finally have this setup right. Thanks Ben.

--- responsive-retrofit-bookmarklet.js
+++ responsive-retrofit-bookmarklet.js
@@ -1,5 +1,5 @@
 javascript: (function () {  
   var jsCode = document.createElement('script');
-  jsCode.setAttribute('src', 'https://raw.github.com/phanza/Responsive-Retrofitting/gh-pages/responsive-retrofit.js');
+  jsCode.setAttribute('src', 'http://phanza.github.com/Responsive-Retrofitting/responsive-retrofit.js');
   document.body.appendChild(jsCode);  
 }());
--- responsive-retrofit.js
+++ responsive-retrofit.js
@@ -1,7 +1,7 @@
 var cssLink = document.createElement('link');
 cssLink.setAttribute('rel', 'stylesheet');
 cssLink.setAttribute('type', 'text/stylesheet');
-cssLink.setAttribute('href', "https://github.com/phanza/Responsive-Retrofitting/blob/gh-pages/" + document.location.hostname + "/css/retrofit.css");
+cssLink.setAttribute('href', "http://phanza.github.com/Responsive-Retrofitting/" + document.location.hostname + "/css/retrofit.css");
 document.head.appendChild(cssLink);
 
 
