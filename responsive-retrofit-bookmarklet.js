911a06e71df2c19e34aa368cecc2bb57142e2e0f
Philip <philip@phanza.com>
2012-07-08 12:52:54 -0400

    Reverted the bookmarklet-related .js files to the master.

--- responsive-retrofit-bookmarklet.js
+++ responsive-retrofit-bookmarklet.js
@@ -1,5 +1,5 @@
 javascript: (function () {  
   var jsCode = document.createElement('script');
-  jsCode.setAttribute('src', 'http://phanza.github.com/Responsive-Retrofitting/responsive-retrofit.js');
+  jsCode.setAttribute('src', 'http://sparkbox.github.com/Responsive-Retrofitting/responsive-retrofit.js');
   document.body.appendChild(jsCode);  
 }());
