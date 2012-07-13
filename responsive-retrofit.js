c72f1e9297a0d77b2c95cc3789a263eca39b0626
John Weis <weis.john@gmail.com>
2012-07-13 13:20:52 -0400

    wrapping JS in closure to not pollute the global variable namespace

--- responsive-retrofit.js
+++ responsive-retrofit.js
@@ -1,12 +1,15 @@
-var cssLink = document.createElement('link');
-cssLink.setAttribute('rel', 'stylesheet');
-cssLink.setAttribute('type', 'text/stylesheet');
-cssLink.setAttribute('href', "http://sparkbox.github.com/Responsive-Retrofitting/" + document.location.hostname + "/css/retrofit.css");
-document.head.appendChild(cssLink);
+(function() {
 
+	var cssLink = document.createElement('link');
+	cssLink.setAttribute('rel', 'stylesheet');
+	cssLink.setAttribute('type', 'text/stylesheet');
+	cssLink.setAttribute('href', "http://weisjohn.github.com/Responsive-Retrofitting/" + document.location.hostname + "/css/retrofit.css");
+	document.head.appendChild(cssLink);
 
-// Add meta tag, too!
-var metaCode = document.createElement('meta');
-metaCode.setAttribute('name', 'viewport');
-metaCode.setAttribute('content', 'width=device-width,initial-scale=1.0');
-document.head.appendChild(metaCode);
+	// Add meta tag, too!
+	var metaCode = document.createElement('meta');
+	metaCode.setAttribute('name', 'viewport');
+	metaCode.setAttribute('content', 'width=device-width,initial-scale=1.0');
+	document.head.appendChild(metaCode);
+
+})();
