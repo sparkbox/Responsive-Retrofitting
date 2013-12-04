1619d45776d6cddf2a8a408fa34b98697a06d667
Ben Callahan <ben@heysparkbox.com>
2013-12-03 21:50:44 -0500

    Revert 26c495e..f3aa40f
    This rolls back to commit 26c495ea1cacfdde86acdbc7aa760bdab2289566.

--- responsive-retrofit.js
+++ responsive-retrofit.js
@@ -3,7 +3,7 @@
 	var cssLink = document.createElement('link');
 	cssLink.setAttribute('rel', 'stylesheet');
 	cssLink.setAttribute('type', 'text/stylesheet');
-	cssLink.setAttribute('href', "https://sparkbox.github.com/Responsive-Retrofitting/" + document.location.hostname + "/css/retrofit.css");
+	cssLink.setAttribute('href', "http://sparkbox.github.com/Responsive-Retrofitting/" + document.location.hostname + "/css/retrofit.css");
 	document.head.appendChild(cssLink);
 
 	// Add meta tag, too!
